c4ddd004c0238ae79d23e5515053551dbcb044eb
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-08-11 00:18:09 +0200

    Add templates

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 use rocket::fs::FileServer;
-use rocket::launch;
+use rocket::{get, launch, routes};
+use rocket_dyn_templates::{context, Template};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", FileServer::from("public"))
+    rocket::build()
+        .mount("/", routes![index, invite, register])
+        .mount("/", FileServer::from("public"))
+        .attach(Template::fairing())
+}
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context! { active_page: "home" })
+}
+
+#[get("/invite")]
+fn invite() -> Template {
+    Template::render("invite", context! { active_page: "invite" })
+}
+
+#[get("/register")]
+fn register() -> Template {
+    Template::render("register", context! { active_page: "register" })
 }
