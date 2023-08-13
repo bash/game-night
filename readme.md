344ad90ff3e7dbdf9f965f42426f459bf322d70c
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-08-13 18:22:45 +0200

    Add email template

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Tau's Game Night
+
+## Generating Emails
+
+```shell
+cargo run --bin render_email <filename>
+# Example:
+cargo run --bin render_email poll.html.tera
+```
---
+++ src/bin/render_email.rs
@@ -0,0 +1,29 @@
+use lettre::{message::header::ContentType, FileTransport, Message, Transport};
+use tera::{Context, Tera};
+
+fn main() {
+    let template_name = std::env::args()
+        .nth(1)
+        .expect("Exactly one argument is required");
+    let mut tera = Tera::new("emails/*.tera").unwrap();
+    tera.build_inheritance_chains().unwrap();
+    let context = build_context();
+    let email = Message::builder()
+        .from("Tau's Game Night <game-night@tau.soy>".parse().unwrap())
+        .to("Example Recipient <recipient@example.com>".parse().unwrap())
+        .subject("Tau's Game Night")
+        .header(ContentType::TEXT_HTML)
+        .body(tera.render(&template_name, &context).unwrap())
+        .unwrap();
+    FileTransport::new("emails").send(&email).unwrap();
+}
+
+fn build_context() -> Context {
+    let mut context = Context::new();
+    context.insert("greeting", "Hi");
+    context.insert("name", "Tau");
+    context.insert("poll_url", "https://www.example.com");
+    context.insert("poll_close_date", "30. September 2023");
+    context.insert("poll_close_time", "5:30 PM");
+    context
+}
