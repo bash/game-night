0c43cf7a807d916f5dc4b520025f8241c691b8d7
Tau <4602612+bash@users.noreply.github.com>
2023-09-10 23:15:58 +0200

    Add new poll page skeleton

--- src/poll.rs
+++ src/poll.rs
@@ -1,13 +1,19 @@
+use crate::authorization::{AuthorizedTo, ManagePoll};
 use crate::template::{PageBuilder, PageType};
 use crate::users::User;
 use rocket::{get, routes, Route};
 use rocket_dyn_templates::{context, Template};
 
 pub(crate) fn routes() -> Vec<Route> {
-    routes![poll_page]
+    routes![poll_page, new_poll_page]
 }
 
 #[get("/poll")]
 fn poll_page(page: PageBuilder<'_>, _user: User) -> Template {
     page.type_(PageType::Poll).render("poll", context! {})
+}
+
+#[get("/poll/new")]
+fn new_poll_page(page: PageBuilder<'_>, _user: AuthorizedTo<ManagePoll>) -> Template {
+    todo!()
 }
