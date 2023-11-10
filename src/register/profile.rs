84fb6ce10aa9e006940a5d6136f7650a3139b9de
Tau <4602612+bash@users.noreply.github.com>
2023-11-10 21:43:45 +0100

    Add subscribe button

--- src/register/profile.rs
+++ src/register/profile.rs
@@ -1,20 +1,24 @@
 use crate::database::Repository;
+use crate::push::WebPushKey;
 use crate::template::PageBuilder;
 use crate::users::rocket_uri_macro_list_users;
 use crate::users::{User, UserPatch};
 use anyhow::{Error, Result};
+use base64::{engine::general_purpose, Engine as _};
 use rocket::form::Form;
 use rocket::response::{Debug, Redirect};
 use rocket::{get, post, uri};
 use rocket_dyn_templates::{context, Template};
 
 #[get("/profile")]
-pub(crate) fn profile(page: PageBuilder, user: User) -> Template {
+pub(crate) fn profile(page: PageBuilder, user: User, push_key: &WebPushKey) -> Template {
+    let push_key = general_purpose::URL_SAFE_NO_PAD.encode(push_key.public_key());
     page.render(
         "register/profile",
         context! {
             can_update_name: user.can_update_name(),
             list_users_uri: user.can_manage_users().then(|| uri!(list_users())),
+            push_key,
         },
     )
 }
