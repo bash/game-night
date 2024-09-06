219c06ce5b2e4fc7891d0d149b7da8c89d98258f
Tau Gärtli <git@tau.garden>
2024-09-06 12:30:53 +0200

    Replace parallel with mprocs

--- make-scripts/run.sh
+++ make-scripts/run.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-export CARGO_TERM_COLOR=always
+cargo install mprocs --quiet
 
 if [[ -d ../outbox ]]; then
-    parallel --lb --halt now,done=1 --tagstring [{}] ::: "$MAKE run_server" "$MAKE run_outbox" "$MAKE watch"
+    mprocs "$MAKE run_server" "$MAKE run_outbox" "$MAKE watch"
 else
     echo "$(tput bold)$(tput setaf 3)Warning: outboxd not started, you need to start it yourself if you want to send emails$(tput sgr0)"
-    "$MAKE" run_server
+    mprocs "$MAKE run_server" "$MAKE watch"
 fi
