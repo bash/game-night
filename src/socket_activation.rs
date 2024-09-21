8601c00eb4d5b0f95a765211720cc304ecb49666
Tau Gärtli <git@tau.garden>
2024-09-22 01:05:26 +0200

    Set listener to nonblocking mode

--- src/socket_activation.rs
+++ src/socket_activation.rs
@@ -5,5 +5,10 @@ use std::io;
 pub(crate) fn listener_from_env() -> io::Result<Option<TcpListener>> {
     ListenFd::from_env()
         .take_tcp_listener(0)
-        .and_then(|r| r.map(TcpListener::from_std).transpose())
+        .and_then(|r| r.map(from_std).transpose())
+}
+
+fn from_std(listener: std::net::TcpListener) -> io::Result<TcpListener> {
+    listener.set_nonblocking(true)?;
+    TcpListener::from_std(listener)
 }
