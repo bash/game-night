d5e599acc5273c30c3d41d4c15b35b30191df751
Tau Gärtli <git@tau.garden>
2025-03-05 19:25:50 +0100

    Use entity state pattern for location

--- src/event/location.rs
+++ src/event/location.rs
@@ -1,6 +1,9 @@
+use crate::database::Materialized;
+use crate::entity_state;
+
 #[derive(Debug, Clone, sqlx::FromRow, serde::Serialize)]
-pub(crate) struct Location<Id = i64> {
-    pub(crate) id: Id,
+pub(crate) struct Location<S: LocationState = Materialized> {
+    pub(crate) id: S::Id,
     pub(crate) nameplate: String,
     pub(crate) street: String,
     pub(crate) street_number: String,
@@ -8,4 +11,10 @@ pub(crate) struct Location<Id = i64> {
     pub(crate) city: String,
     #[sqlx(try_from = "i64")]
     pub(crate) floor: i8,
+}
+
+entity_state! {
+    pub(crate) trait LocationState {
+        type Id = () => i64 => i64;
+    }
 }
