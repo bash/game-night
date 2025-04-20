5640b5222436598c2a0d85a80f55f0753db19b19
Tau Gärtli <git@tau.garden>
2025-04-20 15:17:27 +0200

    Fix service worker registration

--- public/js/service-worker-registration.js
+++
@@ -1,3 +0,0 @@
-navigator.serviceWorker.register('/service-worker.js', { type: 'module', scope: '/' })
-    .then(registration => registration.update())
-    .catch(error => console.warn('Service worker registration failed', error))
