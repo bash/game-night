c7b7df98e6203ced3415e9b7eefce378fdd3cae1
Tau Gärtli <git@tau.garden>
2025-04-20 15:17:27 +0200

    Add more properties

--- src/push/notification.rs
+++ src/push/notification.rs
@@ -1,3 +1,9 @@
+/// Corresponds with the [`Notification` options][opts]
+/// and some extra fields from the [declarative push spec][decl].
+/// Documentation copied from MDN.
+///
+/// [opts]: https://developer.mozilla.org/en-US/docs/Web/API/Notification/Notification#options
+/// [decl]: https://pr-preview.s3.amazonaws.com/w3c/push-api/pull/385.html#members
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub(crate) struct Notification {
     pub(crate) title: String,
@@ -9,16 +15,31 @@ pub(crate) struct Notification {
     /// A URL.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) icon: Option<String>,
+    /// A URL of the image used to represent the notification when
+    /// there isn't enough space to display the notification itself.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) badge: Option<String>,
+    /// A URL of the image to be displayed in the notification.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) image: Option<String>,
     /// Indicates that a notification should remain active until the
     /// user clicks or dismisses it, rather than closing automatically.
     #[serde(rename = "requireInteraction", skip_serializing_if = "Option::is_none")]
     pub(crate) require_interaction: Option<bool>,
+    /// A boolean value specifying whether the user should be notified
+    /// after a new notification replaces an old one.
+    /// If `true`, then tag also must be set.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) renotify: Option<bool>,
     /// A boolean value specifying whether the notification should be silent,
     /// i.e., no sounds or vibrations should be issued regardless of the device settings.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) silent: Option<bool>,
     #[serde(skip_serializing_if = "Vec::is_empty", default)]
     pub(crate) actions: Vec<NotificationAction>,
+    /// Arbitrary data that you want associated with the notification.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) data: Option<serde_json::Value>,
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
@@ -32,7 +53,6 @@ pub(crate) struct NotificationAction {
     pub(crate) navigate: Option<String>,
 }
 
-/// See: <https://pr-preview.s3.amazonaws.com/w3c/push-api/pull/385.html#members>
 #[derive(Debug, Clone, serde::Serialize)]
 pub(crate) struct PushMessage {
     pub(crate) web_push: WebPushDisambiguator,
