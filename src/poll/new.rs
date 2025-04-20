42d0aef9ac83c2ea29f3da53e814140e04c35f71
Tau Gärtli <git@tau.garden>
2025-04-20 15:17:28 +0200

    Send push notification on poll creation

--- src/poll/new.rs
+++ src/poll/new.rs
@@ -8,6 +8,7 @@ use crate::event::{
     StatefulEvent,
 };
 use crate::iso_8601::Iso8601;
+use crate::push::{Notification, PushMessage, PushSender};
 use crate::register::rocket_uri_macro_profile;
 use crate::template::PageBuilder;
 use crate::uri::UriBuilder;
@@ -280,6 +281,7 @@ auto_resolve! {
     pub(crate) struct NewPollNotificationSender {
         subscribed_users: SubscribedUsers,
         email_sender: Box<dyn EventEmailSender<Polling>>,
+        push_sender: PushSender,
         uri_builder: UriBuilder,
     }
 }
@@ -301,17 +303,25 @@ impl NewPollNotificationSender {
                 manage_subscription_url: sub_url,
             };
             self.email_sender.send(&poll.event, &user, &email).await?;
+            const DATE_FORMAT: &[FormatItem] =
+                format_description!("[day padding:none].\u{2009}[month repr:long padding:none]");
+            self.push_sender
+                .send(
+                    &PushMessage::from(Notification {
+                        body: Some(format!(
+                            "Help pick a date until {}",
+                            poll.open_until.format(DATE_FORMAT).expect("TODO")
+                        )),
+                        navigate: Some(
+                            uri!(crate::event::event_page(id = poll.event.id)).to_string(),
+                        ),
+                        ..Notification::new("New Poll for Tau's Game Night")
+                    }),
+                    user.id,
+                )
+                .await?;
         }
 
         Ok(())
     }
-}
-
-async fn send_poll_emails(
-    mut subscribed_users: SubscribedUsers,
-    mut email_sender: Box<dyn EventEmailSender<Polling>>,
-    uri_builder: UriBuilder,
-    poll: &Poll,
-) -> Result<()> {
-    todo!()
 }
