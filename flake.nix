f53fc532bf01ac7ca97519a82f716cb1212e92f9
Jeremy Stucki <dev@jeremystucki.ch>
2025-04-21 19:30:12 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    alejandra = {
+      url = "github:kamadorueda/alejandra/4.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    alejandra,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          just
+          mkcert
+          podman
+          podman-compose
+          sqlite
+        ];
+      };
+
+      formatter = alejandra.defaultPackage.${system};
+    });
+}
