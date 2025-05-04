d06539240546a5201ccb7f1a752961fcc2768050
Tau Gärtli <git@tau.garden>
2025-05-04 18:26:11 +0200

    Port home page

--- src/home.rs
+++ src/home.rs
@@ -1,11 +1,25 @@
+use crate::template_v2::responder::Templated;
 use crate::{uri, PageBuilder};
 use rocket::get;
-use rocket_dyn_templates::{context, Template};
+use rocket::response::Responder;
+use templates::HomePage;
 
 #[get("/", rank = 20)]
-pub(crate) fn home_page(page: PageBuilder<'_>) -> Template {
-    page.render(
-        "index",
-        context! { getting_invited_uri: uri!(crate::register::getting_invited_page())},
-    )
+pub(crate) fn home_page(page: PageBuilder<'_>) -> impl Responder {
+    Templated(HomePage {
+        getting_invited_uri: uri!(crate::register::getting_invited_page()),
+        ctx: page.build(),
+    })
+}
+
+mod templates {
+    use crate::template_v2::prelude::*;
+    use rocket::http::uri::Origin;
+
+    #[derive(Template, Debug)]
+    #[template(path = "index.html")]
+    pub(crate) struct HomePage {
+        pub(super) getting_invited_uri: Origin<'static>,
+        pub(super) ctx: PageContext,
+    }
 }
