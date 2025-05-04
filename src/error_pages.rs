4292fd2bf276903a30df298b33d7569b33d0b41d
Tau Gärtli <git@tau.garden>
2025-05-04 18:34:34 +0200

    Port error pages

--- src/error_pages.rs
+++ src/error_pages.rs
@@ -1,25 +1,44 @@
+use crate::template_v2::responder::Templated;
 use crate::{HttpResult, PageBuilder};
 use anyhow::anyhow;
 use rocket::request::FromRequest as _;
 use rocket::{catch, catchers, Catcher, Request};
-use rocket_dyn_templates::Template;
+use templates::{ForbiddenPage, NotFoundPage};
 
 pub(crate) fn catchers() -> Vec<Catcher> {
     catchers![not_found, forbidden]
 }
 
 #[catch(404)]
-async fn not_found(request: &Request<'_>) -> HttpResult<Template> {
-    let page = PageBuilder::from_request(request)
+async fn not_found(request: &Request<'_>) -> HttpResult<Templated<NotFoundPage>> {
+    let ctx = PageBuilder::from_request(request)
         .await
-        .success_or_else(|| anyhow!("failed to create page builder"))?;
-    Ok(page.render("errors/404", ()))
+        .success_or_else(|| anyhow!("failed to create page builder"))?
+        .build();
+    Ok(Templated(NotFoundPage { ctx }))
 }
 
 #[catch(403)]
-async fn forbidden(request: &Request<'_>) -> HttpResult<Template> {
-    let page = PageBuilder::from_request(request)
+async fn forbidden(request: &Request<'_>) -> HttpResult<Templated<ForbiddenPage>> {
+    let ctx = PageBuilder::from_request(request)
         .await
-        .success_or_else(|| anyhow!("failed to create page builder"))?;
-    Ok(page.render("errors/403", ()))
+        .success_or_else(|| anyhow!("failed to create page builder"))?
+        .build();
+    Ok(Templated(ForbiddenPage { ctx }))
+}
+
+mod templates {
+    use crate::template_v2::prelude::*;
+
+    #[derive(Template)]
+    #[template(path = "errors/404.html")]
+    pub(super) struct NotFoundPage {
+        pub(super) ctx: PageContext,
+    }
+
+    #[derive(Template)]
+    #[template(path = "errors/403.html")]
+    pub(super) struct ForbiddenPage {
+        pub(super) ctx: PageContext,
+    }
 }
