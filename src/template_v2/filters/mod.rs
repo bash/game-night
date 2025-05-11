85bd32fa7767cb3b0d449b441df5fd766d8aa2d9
Tau Gärtli <git@tau.garden>
2025-05-11 15:20:47 +0200

    Put iso_8601 in its own filter

---
+++ src/template_v2/filters/mod.rs
@@ -0,0 +1,40 @@
+use askama::{FastWritable, NO_VALUES};
+use askama_json::askama;
+use std::fmt;
+
+mod date_time;
+pub(crate) use date_time::*;
+
+pub(crate) fn guillemets<W: FastWritable>(
+    input: W,
+    _: &dyn askama::Values,
+) -> askama::Result<Guillemets<W>> {
+    Ok(Guillemets(input))
+}
+
+pub(crate) struct Guillemets<T>(T);
+
+impl<T> fmt::Display for Guillemets<T>
+where
+    T: FastWritable,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.write_into(f, NO_VALUES).map_err(|_| fmt::Error {})
+    }
+}
+
+impl<T> FastWritable for Guillemets<T>
+where
+    T: FastWritable,
+{
+    fn write_into<W: fmt::Write + ?Sized>(
+        &self,
+        dest: &mut W,
+        values: &dyn askama::Values,
+    ) -> askama::Result<()> {
+        write!(dest, "«")?;
+        self.0.write_into(dest, values)?;
+        write!(dest, "»")?;
+        Ok(())
+    }
+}
